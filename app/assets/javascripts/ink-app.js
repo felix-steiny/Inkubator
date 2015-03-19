9019c317ae30c3f56b979e5b2fd5c5632e19c847
felix-steiny <fsteiny@gmail.com>
2015-03-18 23:58:57 -0400

    Now rendering with angular!

--- app/assets/javascripts/ink-app.js
+++ app/assets/javascripts/ink-app.js
@@ -1,10 +1,11 @@
-angular.module('ink', []).controller('ktrl', function($scope){
-    $scope.moose = "meeee";
+angular.module('ink', []).controller('ktrl', function($scope, $http){
 
-    $scope.ideas = [
-        {id: 1, html: "Hello <i>angular<i/>", plaintext: "Hello angular" },
-        {id: 7, html: "<h1>by and by</h1>", plaintext: "by and by" },
-        {id: 30, html: "Fond <i>farewell<i/>", plaintext: "fond farewell" }
-    ]
+    $scope.ideas = [];
+
+    $http.get("/idea")
+        .success(function(data) {
+            $scope.ideas = data;
+        });
+
 });
 
